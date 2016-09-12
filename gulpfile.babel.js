557c06ba1254b80d8362669ba09f5818ca77981a
dafi@nodes.dk <dafi@nodes.dk>
2016-09-12 17:23:03 +0200

    encode64 cloud image; add validation for window resize

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,7 @@ import concat from 'gulp-concat';
 import babel from 'gulp-babel';
 import uglify from 'gulp-uglify';
 import browserSync from 'browser-sync';
+import sass from 'gulp-sass';
 
 gulp.task('scripts', ()=> {
 	return gulp.src('./src/*.js')
@@ -13,7 +14,7 @@ gulp.task('scripts', ()=> {
 		.pipe(uglify({preserveComments: 'some'}))
 		.pipe(gulp.dest('./dist'));
 });
-gulp.task('serve', ['scripts'],()=> {
+gulp.task('serve', ['scripts', 'sass'],()=> {
 	browserSync({
 		notify: false,
 		files: ['./index.html', './dist/*.*'],
@@ -21,6 +22,12 @@ gulp.task('serve', ['scripts'],()=> {
 			baseDir: '.'
 		}
 	})
-	gulp.watch('./src/*.js',['scripts'])
-})
+	gulp.watch('./src/*.js',['scripts']);
+		gulp.watch('./src/*.scss', ['sass']);
+});
+gulp.task('sass', function () {
+	return gulp.src('./src/**/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('./dist'))
+});
 gulp.task('default', ['scripts']);
